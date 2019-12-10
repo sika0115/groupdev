dedec752fa9b7ae86373b197c113da60e420933e
sika <36564217+sika0115@users.noreply.github.com>
2019-12-10 12:09:30 +0900

    完成

--- VBA-1/sheet1_1.vb
+++ VBA-1/sheet1_1.vb
@@ -13,14 +13,16 @@ Dim オプション As Long
 Dim ソース情報 As String
 Dim 項目数 As Long
 Dim レコード数 As Long
-Dim ソース情報 As String
-Dim Row　As Long '//行
-'//Dim phone_number As String
- 
+Dim Row As Long
+Const COLL_CODE As Integer = 1
+Const COLL_NAME As Integer = 2
+Const COLL_ADDRESS As Integer = 3
+Const COLL_PHONE As Integer = 4
+Const COLL_LABORS As Integer = 5
 '//(1)
 Private Sub CommandButton1_Click()
 '//接続
-Set 接続 = New ADODB.Connection
+Set 接続 = New ADODB.Connection 
 ユーザID = "sk16100"
 パスワード = "sk16100"
 サーバ名 = "jocalc1"
@@ -36,16 +38,15 @@ Set 接続 = New ADODB.Connection
 権限モード = adModeRead '//書込= adModeWrite，更新= adModeReadWrite
 接続.Mode = 権限モード
 接続.Open               '//  同期接続のみとする(非同期接続は不使用)
-
 '//レコードセットオープン
-Set  レコードセット = New ADODB.Recordset '//SQL発行後データ格納
+Set レコードセット = New ADODB.Recordset  '//SQL発行後データ格納
 
 '//カーソルエンジンの設定
-カーソルエンジン = adUseClient  '// クライアント側=adUseClient，サーバ側=adUseServer 
+カーソルエンジン = adUseClient  '// クライアント側=adUseClient，サーバ側=adUseServer
 レコードセット.CursorLocation = カーソルエンジン
-カーソルタイプ = adOpenDynamic  '// adOpenKeyset，adOpenStatic，adOpenForwardOnly 
-ロック情報 = adLockOptimistic   '// adLockReadOnly，adLockPessimistic 
-オプション= adCmdText           '// adCmdUnknown，adCmdTableDirect
+カーソルタイプ = adOpenDynamic  '// adOpenKeyset，adOpenStatic，adOpenForwardOnly
+ロック情報 = adLockOptimistic   '// adLockReadOnly，adLockPessimistic
+オプション = adCmdText          '// adCmdUnknown，adCmdTableDirect
 ソース情報 = "select * from company_19"
 
 レコードセット.Open ソース情報, 接続, カーソルタイプ, ロック情報, オプション
@@ -53,27 +54,18 @@ Set  レコードセット = New ADODB.Recordset '//SQL発行後データ格納
 '//レコード追加、更新
 For Row = 2 To 119 Step 1
     レコードセット.AddNew
-    レコードセット.Fields("code").Value = Worksheets("データ").Cells(Row, 1).Value   '//企業コード : code
-    レコードセット.Fields("name").Value = Worksheets("データ").Cells(Row, 2).Value   '//企業名 : name
-    レコードセット.Fields("address").Value = Worksheets("データ").Cells(Row, 3).Value'//所在地 : address
-    レコードセット.Fields("phone").Value = Worksheets("データ").Cells(Row, 4).Value  '//電話番号 : phone
-    レコードセット.Fields("labours").Value = Worksheets("データ").Cells(Row, 5).Value'//従業員数 : labours
+    レコードセット.Fields("code").Value = Worksheets("データ").Cells(Row, COLL_CODE).Value   '//企業コード : code
+    レコードセット.Fields("name").Value = Worksheets("データ").Cells(Row, COLL_NAME).Value   '//企業名 : name
+    レコードセット.Fields("address").Value = Worksheets("データ").Cells(Row, COLL_ADDRESS).Value '//所在地 : address
+    レコードセット.Fields("phone").Value = Worksheets("データ").Cells(Row, COLL_PHONE).Value  '//電話番号 : phone
+    レコードセット.Fields("labors").Value = Worksheets("データ").Cells(Row, COLL_LABORS).Value '//従業員数 : labors
     
-    '//電話番号の処理
-    '//If IsNull(Sheet1.Cells(Row, "D").Value) Then
-       '// phone_number = ""
-    '//Else
-       '// phone_number = Sheet1.Cells(Row, "D").Value
-    '//End If
-    '//レコードセット.Fields("phone").Value = phone_number'//電話番号 : phone
-
     レコードセット.Update
 Next
 
 レコードセット.Close
 接続.Close
-MsgBox("company_19に格納完了")
-
+MsgBox ("company_19に格納完了")
 End Sub
 
 '//(2)
@@ -97,14 +89,14 @@ Set 接続 = New ADODB.Connection
 接続.Open               '//  同期接続のみとする(非同期接続は不使用)
 
 '//レコードセットオープン
-Set  レコードセット = New ADODB.Recordset '//SQL発行後データ格納
+Set レコードセット = New ADODB.Recordset  '//SQL発行後データ格納
 
 '//カーソルエンジンの設定
-カーソルエンジン = adUseClient  '// クライアント側=adUseClient，サーバ側=adUseServer 
+カーソルエンジン = adUseClient  '// クライアント側=adUseClient，サーバ側=adUseServer
 レコードセット.CursorLocation = カーソルエンジン
-カーソルタイプ = adOpenDynamic  '// adOpenKeyset，adOpenStatic，adOpenForwardOnly 
-ロック情報 = adLockOptimistic   '// adLockReadOnly，adLockPessimistic 
-オプション= adCmdText           '// adCmdUnknown，adCmdTableDirect
+カーソルタイプ = adOpenDynamic  '// adOpenKeyset，adOpenStatic，adOpenForwardOnly
+ロック情報 = adLockOptimistic   '// adLockReadOnly，adLockPessimistic
+オプション = adCmdText          '// adCmdUnknown，adCmdTableDirect
 ソース情報 = "select * from company_19"
 
 '//レコード削除
@@ -115,6 +107,6 @@ For Row = 2 To 119 Step 1
 Next
 
 接続.Close
-MsgBox("company_19のレコードを全消去")
+MsgBox ("company_19のレコードを全消去")
 
 End Sub
