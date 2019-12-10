af2c3627add7a689367710cb7f3058e72fac7c90
sika <36564217+sika0115@users.noreply.github.com>
2019-12-10 13:22:34 +0900

    new php file upload

---
+++ form.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional //EN">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+<title>企業一覧表</title>
+</head>
+<body bgcolor=#d8e698>
+
+
+
+<h2><font face="ＭＳ ゴシック" color=#2b2b2b>　企業一覧表</font></h2>
+<hr>
+<form name="frm" method="post" action="login.php" target="_top">
+<font face="ＭＳ ゴシック">企業名部分一致検索</font><input type="text" name="srv" value="" size="20" maxlength="80" style="font-family: ＭＳ 明朝; font-size: 11pt; color: black; background-color: white; ime-mode: auto"><br>
+<br>
+<font face="ＭＳ 明朝">
+　　　　　　　　<input type="button" value="検索" > 
+<!--onClick="Input_Search(frm.srv, frm.uid, frm.pas, frm.dbm)"> -->
+
+</form>
+<hr>
+</body>
+</font>
+</html>
